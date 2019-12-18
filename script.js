039d49f5d0e06887a4c4d931bc7811ecefa6f299
Natalie Carr <natticarr@gmail.com>
2019-12-18 14:17:15 +1100

    added Add to Deck functionality to sidebar and save deck functionality

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ $(".modalAdvanceButtons").click(function () {
   $(this).css('border-radius', '2em');
 });
 */
-$(".color-filter").click(function(){
+$(".color-filter").click(function () {
   $(this).css('border', '2px solid yellow');
   $(this).css('border-radius', '3em');
 })
@@ -42,11 +42,15 @@ function openNav() {
   document.getElementById("mySidebar").style.width = "350px";
   document.getElementById("mySidebar").style.marginTop = "70px";
   document.getElementById("collection").style.marginRight = "350px";
+  addToDeckToggle = 1; //toggle for card adding to deck
+ 
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("collection").style.marginRight = "auto";
+  addToDeckToggle = 0; //toggle for card adding to deck
+  
 }
 
 /* Get the documentElement (<html>) to display the page in fullscreen */
@@ -55,14 +59,37 @@ var elem = document.documentElement;
 function openFullscreen() {
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { 
+  } else if (elem.mozRequestFullScreen) {
     elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { 
+  } else if (elem.webkitRequestFullscreen) {
     elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { 
+  } else if (elem.msRequestFullscreen) {
     elem.msRequestFullscreen();
   }
 }
+
+
+///////////////// Add to deck part /////////////////
+var deck;
+var deckString;
+var deckCount;
+var addToDeckToggle;
+var deckCountString;
+
+if (localStorage.getItem("deckString") === null) {
+  deck = [];
+  deckCount = [];
+
+}
+else {
+
+  deckString = localStorage.getItem("deckString");
+  deckCountString = localStorage.getItem("deckCountString");
+  deck = JSON.parse(deckString);
+  deckCount = JSON.parse(deckCountString);
+  buildDeck(); //build first deck using local storage
+};
+/////////////////////////////////////
 
 var apiUrl = "https://api.scryfall.com/cards/search?order=cmc&unique=cards&q=e"
 var apiUrl2 = "https://api.scryfall.com/cards/search?order=cmc&page=2&unique=cards&q=e"
@@ -134,7 +161,7 @@ $("#search").on("click", function () {
     display(searchPool);
   }
   else {
-    console.log(searchPool);
+    
     for (var j = 0; j < searchPool.length; j++) {
       if (searchPool[j].type.indexOf(searchTerm) != -1 || searchPool[j].name.indexOf(searchTerm) != -1 || searchPool[j].oracle.indexOf(searchTerm) != -1 || searchPool[j].oracle.search(input) != -1) {
         secondSearch.push({
@@ -159,7 +186,7 @@ function addFilters(buttonClass, filterArray) {
     $(".new-slide").remove();
     var filter = $(this).attr("value");
     if (filterArray.indexOf(filter) == -1) {
-      $(this).attr("style","border-color: gold");
+      $(this).attr("style", "border-color: gold");
       if (filter == "7") {
         filterArray.push("7", "8", "9", "10", "11", "12", "13", "14", "15", "16");
       }
@@ -168,8 +195,8 @@ function addFilters(buttonClass, filterArray) {
       }
     }
     else {
-      
-      $(this).attr("style","border-color:white");
+
+      $(this).attr("style", "border-color:white");
       if (filter == "7") {
         filterArray.splice(filterArray.indexOf(filter), 10);
       }
@@ -330,7 +357,7 @@ function sortByColor() {
   cardPool = cardPool.concat(multiColor);
   cardPool = cardPool.concat(colorless);
   cardPool = cardPool.concat(lands);
-  console.log(cardPool);
+
 }
 
 function displaySearch(array, arrayOfFilters, array2) {
@@ -386,10 +413,16 @@ function displaySearch(array, arrayOfFilters, array2) {
     }
   }
   display(array2);
+  $(".card").on("click", function () {
+   
+  }); //on click listener for any card on screen
+  $(".remove-from-deck").unbind().click(removeFromDeck);  //remove from deck on click listener added to buttons
+
 }
 
 function display(array) {
   cardsFound = array.length;
+  
   $(".found").text("Cards Found: " + cardsFound);
   numSlide = array.length / 18;
   var slideActive = false;
@@ -413,7 +446,9 @@ function display(array) {
     for (var j = i * 18; j < (i + 1) * 18; j++) {
       var createImg = $('<img>');
       createImg.attr("style", "background-color:transparent");
+
       createImg.attr("src", array[j].imgUrl);
+     
       createImg.attr("data-cost", array[j].manaCost);
       createImg.attr("data-name", array[j].name);
       for (h = 0; h < array[j].colors.length; h++) {
@@ -421,8 +456,14 @@ function display(array) {
       }
       createImg.addClass("col-md-2 card cardImgs");
       createRow.append(createImg);
+
     }
+
   }
+
+  $(".card").on("click", addCardToDeck);
+
+
 }
 function API_CALL(url1, url2) {
   $.ajax({
@@ -430,17 +471,68 @@ function API_CALL(url1, url2) {
     method: "GET"
   }).then(function (response) {
     responseData = response.data;
+    
     $.ajax({
       url: url2,
       //the api call only returns 175 results, therefore a second api call for the second page of the same search term is required
       method: "GET"
     }).then(function (feed) {
       var newResponseData = feed.data;
+     
       responseData = responseData.concat(newResponseData);
       sortByColor();
-      display(cardPool);
+      
+      
+      cardsFound = cardPool.length;
+      
+      $(".found").text("Cards Found: " + cardsFound);
+      numSlide = cardPool.length / 18;
+      var slideActive = false;
+      for (var i = 0; i < Math.ceil(numSlide); i++) {
+        var createCarousel = $('<div>');
+        createCarousel.addClass("carousel-item")
+        if (slideActive == false) {
+          createCarousel.addClass("active new-slide");
+          slideActive = true;
+        }
+        else {
+          createCarousel.addClass("new-slide")
+        }
+        var createContainer = $('<div>');
+        var createRow = $('<div>');
+        createContainer.addClass("container");
+        createRow.addClass("row");
+        $(".carousel-inner").append(createCarousel);
+        createCarousel.append(createContainer);
+        createContainer.append(createRow);
+        for (var j = i * 18; j < (i + 1) * 18; j++) {
+          var createImg = $('<img>');
+          createImg.attr("style", "background-color:transparent");
+
+          createImg.attr("src", cardPool[j].imgUrl);
+          
+          createImg.attr("data-cost", cardPool[j].manaCost);
+          createImg.attr("data-name", cardPool[j].name);
+          for (h = 0; h < cardPool[j].colors.length; h++) {
+            createImg.addClass(cardPool[j].colors[h]);
+          }
+          createImg.addClass("col-md-2 card cardImgs");
+          createRow.append(createImg);
+
+        }
+
+      }
+      $(".card").on("click", addCardToDeck);
+      $(".remove-from-deck").unbind().click(removeFromDeck);  //remove from deck on click listener added to buttons
+
     });
+    //on click listener for any card on screen
+    $(".card").on("click", addCardToDeck);
+    $(".remove-from-deck").unbind().click(removeFromDeck);  //remove from deck on click listener added to buttons
+
   });
+  $(".card").on("click", addCardToDeck);
+  $(".remove-from-deck").unbind().click(removeFromDeck);  //remove from deck on click listener added to buttons
 }
 
 $(".left").on("click", function () {
@@ -450,3 +542,80 @@ $(".right").on("click", function () {
   $("#carouselExampleControls").carousel("next");
 });
 API_CALL(QueryURL, QueryURL2);
+
+/////////////////////////// add card to deck functionality //////////////////////
+
+
+
+function addCardToDeck() {
+
+  if (addToDeckToggle === 1) {
+    var cardChosen = $(this).attr("data-name"); //set button text to be the name of the card
+
+    
+    var indexOfCopy = deck.indexOf(cardChosen); //searches through deck array to find if card chosen is same as existing deck card
+    if (indexOfCopy < 0) { //if chosen card is not already in deck, add one to the end of the deckCount array
+      deckCount.push(1);
+      deck.push(cardChosen); //add chosen card to the end of the deck array
+    }
+    else {
+      deckCount[indexOfCopy] = deckCount[indexOfCopy] + 1;
+      if (deckCount[indexOfCopy] === 5) { //dont let there be more than 4 copies of the same card
+        deckCount[indexOfCopy] = 4;
+      }
+    }
+    buildDeck(); //runs buildDeck function after card has been added to deck array
+  }
+};
+
+function buildDeck() {
+
+  $(".deck-collection").text(""); //clear the temporary built deck every time function is run
+  for (var j = 0; j < deck.length; j++) { //goes through length of deck and creates a button for each card
+    var cardBtn = $("<button>");
+    var cardCountBtn = $("<button>");
+    cardBtn.text(deck[j]);
+    cardBtn.addClass("btn btn-dark");
+    cardCountBtn.text(deckCount[j]);
+    cardCountBtn.addClass("btn btn-danger");
+    cardCountBtn.css("padding-right: 5px") //cardCountBtn is a counter that tells user how many of the same card is in their deck
+    $(".deck-collection").append(cardCountBtn);
+    $(".deck-collection").append(cardBtn);
+    $(".deck-collection").append($("<p>"));
+    cardBtn.addClass("remove-from-deck");
+    cardCountBtn.attr("data-name", deck[j]);
+    cardCountBtn.addClass("card-counter");
+    $(".remove-from-deck").unbind().click(removeFromDeck); //on click for each card button, the remove from deck functionality is added
+  };
+};
+
+function removeFromDeck() {
+
+  var removedCardName = $(this).text(); //get name of card user wants to remove
+  var index = deck.indexOf(removedCardName); //searches deck array for card name
+
+  if (deckCount[index] === 1) { //if only one copy of that card exists, then remove it from the array and from the UI
+    deck.splice(index, 1);
+    deckCount.splice(index, 1); //remove deck count element from array
+    $(".card-counter[data-name='" + $(this).text() + "']").remove(); //remove card counter from UI for specific button
+    $(this).remove();
+  }
+  else {
+    deckCount[index] = deckCount[index] - 1; //if multiple copies of card exist, then remove only one from the existing card count
+    $(".card-counter[data-name='" + $(this).text() + "']").text(deckCount[index]);
+  }
+};
+
+$(".saveButton").on("click", function () { //for save deck button
+  deckString = JSON.stringify(deck);
+  localStorage.setItem("deckString", deckString);
+  deckCountString = JSON.stringify(deckCount);
+  localStorage.setItem("deckCountString", deckCountString);
+})
+
+
+
+
+
+
+
