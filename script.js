6480f6e1830f2370b5ca523fcf1592aff9765416
jasper <msavantfou@gmail.com>
2019-12-22 13:14:35 +1100

    refined multicolor filter

--- script.js
+++ script.js
@@ -23,9 +23,7 @@ var classColor = ".color-filter";
 var classType = ".type-filter";
 var classRarity = ".rarity-filter";
 var classCost = ".number-filter";
-var featureArray2 = [];
 var featureArray = [];
-var searchTermFilter = [];
 var multiColorFilter = [];
 var colorFilter = [];
 var costFilter = [];
@@ -43,6 +41,7 @@ var colorless = [];
 var lands = [];
 var searchPool = [];
 var secondSearch = []
+// firstSearch and firstTypeSearch are switches to direct the search method to either search on the base level or search on the refined level
 var firstSearch = true;
 var firstTypeSearch = true;
 
@@ -51,17 +50,8 @@ $("#search").on("click", function () {
   $(".new-slide").remove();
   var input = $("#input").val();
   var searchTerm = input.charAt(0).toUpperCase() + input.slice(1);
-  searchTermFilter.push(input, searchTerm);
-});
-
-
-
-$("#search").on("click", function () {
-  $(".card").remove();
-  $(".new-slide").remove();
-  var input = $("#input").val();
-  var searchTerm = input.charAt(0).toUpperCase() + input.slice(1);
   if (firstSearch) {
+    // when no filter button has been selected, the type search will search from all of the cards
     searchPool = [];
     for (var i = 0; i < responseData.length; i++) {
       if (responseData[i].type_line.indexOf(searchTerm) != -1 || responseData[i].name.indexOf(searchTerm) != -1 || responseData[i].oracle_text.indexOf(searchTerm) != -1 || responseData[i].oracle_text.search(input) != -1) {
@@ -81,6 +71,7 @@ $("#search").on("click", function () {
     display(searchPool);
   }
   else {
+    // when at least one filter button has been selected, the type search will search in a refined pool defined by the selected filters
     console.log(searchPool);
     for (var j = 0; j < searchPool.length; j++) {
       if (searchPool[j].type.indexOf(searchTerm) != -1 || searchPool[j].name.indexOf(searchTerm) != -1 || searchPool[j].oracle.indexOf(searchTerm) != -1 || searchPool[j].oracle.search(input) != -1) {
@@ -106,8 +97,18 @@ function addFilters(buttonClass, filterArray) {
     $(".new-slide").remove();
     var filter = $(this).attr("value");
     if (filterArray.indexOf(filter) == -1) {
-      $(this).attr("style","border-color: gold");
+      //populate 5 filter arrays with values based on which filter button has been selected
+      if ($(this).attr("class").indexOf(classColor) != -1) {
+        $("."+filter).attr("style", "border-color: gold");
+      }
+      else {
+        $(this).attr("style", "border-color: gold");
+      }
+      
+      console.log( $("."+filter).attr("style"));
+      console.log(filter);
       if (filter == "7") {
+        // special consideration for the option "7+" in the number filter
         filterArray.push("7", "8", "9", "10", "11", "12", "13", "14", "15", "16");
       }
       else {
@@ -115,21 +116,34 @@ function addFilters(buttonClass, filterArray) {
       }
     }
     else {
-      
-      $(this).attr("style","border-color:white");
+      if ($(this).attr("class").indexOf(classColor) != -1) {
+        $("."+filter).attr("style", "border-color: white");
+      }
+      else {
+        // if the same filter has been selected, then the next click will de-select it
+        $(this).attr("style", "border-color:white");
+      }
+
       if (filter == "7") {
+        // special consideration for the option "7+"
         filterArray.splice(filterArray.indexOf(filter), 10);
       }
       else {
         filterArray.splice(filterArray.indexOf(filter), 1);
       }
+      
+    console.log( $("."+filter).attr("style"));
+    console.log(filter);
     }
+    
     if (firstTypeSearch) {
+      // if no type search has been performed, the filter buttons will filter all cards
       searchPool = [];
       firstSearch = false;
       displaySearch(cardPool, arrayOfFilters, searchPool);
     }
     else {
+      // if a type search has been performed, the filter buttons will only filter the search result
       secondSearch = [];
       displaySearch(searchPool, arrayOfFilters, secondSearch);
     }
@@ -158,24 +172,28 @@ $("#go").on("click", function () {
 $("#reset").on("click", function () {
   $(".card").remove();
   $(".new-slide").remove();
+  $("button").attr("style","border-color: white");
   searchPool = [];
   secondSearch = [];
   firstSearch = true;
   firstTypeSearch = true;
-  colorFilter = [];
-  costFilter = [];
-  rarityFilter = [];
-  typeFilter = [];
+  colorFilter.splice(0, colorFilter.length);
+  costFilter.splice(0, costFilter.length);
+  rarityFilter.splice(0, rarityFilter.length);
+  typeFilter.splice(0, typeFilter.length);
   featureArray = [];
-  filterArray = [];
+  multiColorFilter.splice(0, multiColorFilter.length);
+
   console.log(arrayOfFilters);
   display(cardPool);
 });
 
 function sortByColor() {
+  // placing mono-colored cards first then in the order of "white" ,"blue","black","red","green","multicolor","colorless" and "lands"
   for (var i = 0; i < responseData.length; i++) {
     if (responseData[i].colors.length == 1 && responseData[i].colors[0] == "W") {
       white.push({
+        //attaching all searchable key words to each card in the json format
         "imgUrl": responseData[i].image_uris.border_crop,
         "name": responseData[i].name,
         "type": responseData[i].type_line,
@@ -282,6 +300,7 @@ function sortByColor() {
 
 function displaySearch(array, arrayOfFilters, array2) {
   cardsFound = 0;
+  //line 289-310: populate featureArray with searchable key words from the json object attached to each card
   for (var i = 0; i < array.length; i++) {
     featureArray = [];
     if (array[i].colors.length < 1) {
@@ -303,19 +322,53 @@ function displaySearch(array, arrayOfFilters, array2) {
       featureArray.push(words[p]);
     }
     featureArray.push(array[i].rarity);
-    var counter = 0;
-    for (var j = 0; j < arrayOfFilters.length; j++) {
-      if (arrayOfFilters[j].length == 0) {
-        counter++
-      }
-      for (k = 0; k < arrayOfFilters[j].length; k++) {
-        var feature = arrayOfFilters[j][k];
+
+    //line 313-337: set the condition for the colorArray to return a positive result based on whether or not "multercolor" has been selected
+    var counter = 0; //positive results are recorded by var counter
+    if (colorFilter.length == 0) {
+      counter++;
+    }
+    else if (multiColorFilter.length == 0) {
+      //when multicolor is not selected, the search result is the union of selected colors
+      for (var y = 0; y < colorFilter.length; y++) {
+        var feature = colorFilter[y];
         if (featureArray.indexOf(feature) != -1) {
           counter++;
+          break;
         }
       }
     }
-    if (counter >= 5) {
+    else {
+      //when multicolor is selected, the search result is the intersection of selected colors
+      var counter2 = 0;
+      for (var z = 0; z < colorFilter.length; z++) {
+        var feature = colorFilter[z];
+        if (featureArray.indexOf(feature) != -1) {
+          counter2++;
+        }
+      }
+      if (counter2 == colorFilter.length) {
+        counter++;
+      }
+    }
+    // line 339-351: set the condition for the rest of the filters to return a positive result
+    for (var j = 1; j < arrayOfFilters.length; j++) {
+      if (arrayOfFilters[j].length == 0) {
+        counter++
+      }
+      for (var k = 0; k < arrayOfFilters[j].length; k++) {
+        var feature = arrayOfFilters[j][k];
+        if (featureArray.indexOf(feature) != -1) {
+          counter++;
+
+          // all search results for these filters are unions of selected filters
+          break;
+        }
+      }
+    }
+    console.log(counter);
+    // line 353-370: only when all 5 filters return positive will the the card be picked by the search method for display
+    if (counter == 5) {
       cardsFound++;
       array2.push(
         {
@@ -327,7 +380,6 @@ function displaySearch(array, arrayOfFilters, array2) {
           "type": array[i].type,
           "cmc": array[i].cmc,
           "rarity": array[i].rarity
-
         }
       );
     }
@@ -340,12 +392,13 @@ function display(array) {
   $(".found").text("Cards Found: " + cardsFound);
   numSlide = array.length / 18;
   var slideActive = false;
+  //creating a number of carousel slides based on the size of the search result
   for (var i = 0; i < Math.ceil(numSlide); i++) {
     var createCarousel = $('<div>');
     createCarousel.addClass("carousel-item");
     if (slideActive == false) {
+      // assigning "class active" to only one slide for carousel to function properly
       createCarousel.addClass("active new-slide");
-      
       slideActive = true;
     }
     else {
@@ -359,9 +412,11 @@ function display(array) {
     createCarousel.append(createContainer);
     createContainer.append(createRow);
     for (var j = i * 18; j < (i + 1) * 18; j++) {
+      //populating carousel slide with 18 cards
       var createImg = $('<img>');
       createImg.attr("style", "background-color:transparent");
       createImg.attr("src", array[j].imgUrl);
+      //adding usable information for future "on click" events
       createImg.attr("data-cost", array[j].manaCost);
       createImg.attr("data-name", array[j].name);
       for (h = 0; h < array[j].colors.length; h++) {
